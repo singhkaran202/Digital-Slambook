0beb122c64753111204fad3ebd6a2ffb6444dba1
Karandeep Singh <105576642+singhkaran202@users.noreply.github.com>
2022-08-08 11:42:39 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,16 @@ var db=require('../database');
 router.use(express.static(__dirname + '/public'));
  
 router.get('/dashboard/user-list', function(req, res, next) {
+  if (req.session.loggedinUser){
     var sql='SELECT * FROM registration2';
     db.query(sql, function (err, data, fields) {
     if (err) throw err;
     res.render('user-list', { userData: data});
   });
+  }else{
+    res.redirect('/login');
+  }
+  
 });
 
 router.post('/dashboard/user-list', function(req,res){
