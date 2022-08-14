625181f3d1d48e34d4fd5992dd30a6c8f89aed58
Karandeep Singh <105576642+singhkaran202@users.noreply.github.com>
2022-08-14 10:58:45 +0530

    Update comment-route2.js

--- routes/comment-route2.js
+++ routes/comment-route2.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
-var db=require('../database');//one folder back is there a database
-var bodyParser = require("body-parser")
+const express = require('express');
+const router = express.Router();
+const db=require('../database');//one folder back is there a database
+const bodyParser = require("body-parser")
 router.use(bodyParser.urlencoded({extended: true}))
 // router.use(express.static(__dirname + '/public'));
 // to display registration form 
